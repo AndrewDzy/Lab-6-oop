372d74f440a3f895fe951c72a6b576d9d8eb5695
Administrator <Administrator@DESKTOP-OUBGVFD>
2024-03-30 17:44:19 +0200

    Add project files.

---
+++ Lab 6/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿
+namespace Lab_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.coeffATextBox = new System.Windows.Forms.TextBox();
+            this.coeffBTextBox = new System.Windows.Forms.TextBox();
+            this.coeffCTextBox = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.rootsTextBox = new System.Windows.Forms.TextBox();
+            this.solveButton = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(788, 438);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.solveButton);
+            this.tabPage1.Controls.Add(this.rootsTextBox);
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Controls.Add(this.coeffCTextBox);
+            this.tabPage1.Controls.Add(this.coeffBTextBox);
+            this.tabPage1.Controls.Add(this.coeffATextBox);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(780, 412);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // coeffATextBox
+            // 
+            this.coeffATextBox.Location = new System.Drawing.Point(136, 84);
+            this.coeffATextBox.Name = "coeffATextBox";
+            this.coeffATextBox.Size = new System.Drawing.Size(100, 20);
+            this.coeffATextBox.TabIndex = 0;
+            this.coeffATextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.coeffATextBox_KeyPress);
+            // 
+            // coeffBTextBox
+            // 
+            this.coeffBTextBox.Location = new System.Drawing.Point(136, 123);
+            this.coeffBTextBox.Name = "coeffBTextBox";
+            this.coeffBTextBox.Size = new System.Drawing.Size(100, 20);
+            this.coeffBTextBox.TabIndex = 1;
+            this.coeffBTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.coeffBTextBox_KeyPress);
+            // 
+            // coeffCTextBox
+            // 
+            this.coeffCTextBox.Location = new System.Drawing.Point(136, 164);
+            this.coeffCTextBox.Name = "coeffCTextBox";
+            this.coeffCTextBox.Size = new System.Drawing.Size(100, 20);
+            this.coeffCTextBox.TabIndex = 2;
+            this.coeffCTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.coeffCTextBox_KeyPress);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(136, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(423, 33);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // rootsTextBox
+            // 
+            this.rootsTextBox.Location = new System.Drawing.Point(268, 123);
+            this.rootsTextBox.Name = "rootsTextBox";
+            this.rootsTextBox.Size = new System.Drawing.Size(299, 20);
+            this.rootsTextBox.TabIndex = 4;
+            // 
+            // solveButton
+            // 
+            this.solveButton.Location = new System.Drawing.Point(268, 164);
+            this.solveButton.Name = "solveButton";
+            this.solveButton.Size = new System.Drawing.Size(75, 23);
+            this.solveButton.TabIndex = 5;
+            this.solveButton.Text = "button1";
+            this.solveButton.UseVisualStyleBackColor = true;
+            this.solveButton.Click += new System.EventHandler(this.solveButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button solveButton;
+        private System.Windows.Forms.TextBox rootsTextBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox coeffCTextBox;
+        private System.Windows.Forms.TextBox coeffBTextBox;
+        private System.Windows.Forms.TextBox coeffATextBox;
+    }
+}
+
---
+++ Lab 6/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void solveButton_Click(object sender, EventArgs e)
+        {
+            // Отримуємо значення коефіцієнтів a, b та c з TextBoxes
+            double a, b, c;
+            if (!double.TryParse(coeffATextBox.Text, out a) || !double.TryParse(coeffBTextBox.Text, out b) || !double.TryParse(coeffCTextBox.Text, out c))
+            {
+                MessageBox.Show("Будь ласка, введіть числове значення для всіх коефіцієнтів.");
+                return;
+            }
+
+            // Обчислення дискримінанта
+            double discriminant = b * b - 4 * a * c;
+
+            if (discriminant < 0)
+            {
+                // Рівняння не має коренів у множині дійсних чисел
+                rootsTextBox.Text = "Рівняння не має коренів у множині дійсних чисел.";
+            }
+            else if (discriminant == 0)
+            {
+                // Рівняння має один корінь
+                double root = -b / (2 * a);
+                rootsTextBox.Text = $"Рівняння має один корінь: {root}";
+            }
+            else
+            {
+                // Рівняння має два корені
+                double root1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+                double root2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+                rootsTextBox.Text = $"Рівняння має два корені: {root1} та {root2}";
+            }
+        }
+
+        private void coeffATextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '-' || textBox.SelectionStart != 0 || textBox.Text.Contains("-")))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void coeffBTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '-' || textBox.SelectionStart != 0 || textBox.Text.Contains("-")))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void coeffCTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '-' || textBox.SelectionStart != 0 || textBox.Text.Contains("-")))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
